43fd5d109c04a438dab58f64d195864e7e230bb7
hemanuelx <hemanuelx@gmail.com>
2018-03-26 22:44:12 -0300

    Tentativa de correção de botão collapse

--- src/routes/Header.js
+++ src/routes/Header.js
@@ -6,38 +6,37 @@ import {getUser, logout} from '../actions/userActions';
 class Header extends Component {
     render() {
         return (
-            <nav className="navbar navbar-default">
-                <div className="container-fluid">
-                    <div className="navbar-header">
-                        <button class="btn btn-primary" type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar" aria-expanded="false" >
-                            <span className="icon-bar" />
-                            <span className="icon-bar" />
-                            <span className="icon-bar" />
+            <div class="container-fluid">
+            <nav class="navbar navbar-expand-ls navbar-light bg-light">
+                <div className="container-fluid">                    
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" 
+                                data-target="#myNavbar" aria-controls="myNavbar" 
+                                aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
                         </button>
 
                         <Link className="navbar-brand" to="/">Diariy2018</Link>
-                    </div>
-                    <div className="collapse navbar-collapse" id="myNavbar">
-                        <ul className="nav navbar-nav navbar-right">
-                        {
-                            this.props.user === null ? (
-                                <li><Link to="/Login">Login</Link></li>Header
-                            ) : (
-                                <li><Link to="/logout">Logout</Link></li>
-                            )
-                        }
-                            
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+                    </div>                                    
+            </nav>            
+            <div className="collapse" id="myNavbar">
+                <ul className="nav navbar-nav navbar-right">
+                {
+                    this.props.user === null ? (
+                        <li><Link to="/Login">Login</Link></li>
+                    ) : (
+                        <li><Link to="/logout">Logout</Link></li>
+                    )
+                }                            
+                </ul>
+            </div>
+            </div>
         );
     }
 }
 
 function mapStateToProps(state, ownProps) {
     return {
-        user: state.user;   
+        user: state.user
     }
 }
 
