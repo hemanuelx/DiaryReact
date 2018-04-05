035fc3a5e50e96a537e5e0e468846f91acee0f82
hemanuelx <hemanuelx@gmail.com>
2018-04-05 10:00:44 -0300

    Update index.js
    Correção de escrita

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.jscd ';
+import 'bootstrap/dist/js/bootstrap.min.js';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
